04a7ca9e9442f515b60daa9ec919d927f606944f
parktaehoon1 <92parkth@gmail.com>
2022-10-20 14:29:53 +0900

    헤더 작업중

--- js/main.js
+++ js/main.js
@@ -1,90 +1,78 @@
 $(document).ready(function () {
- // 모달창
- let modalWrap = $(".modal-wrap");
- let modalClose = $(".modal-close");
+  // 모달창
+  let modalWrap = $(".modal-wrap");
+  let modalClose = $(".modal-close");
 
- modalClose.click(function () {
-   modalWrap.stop().fadeOut(500);
-   // 추가기능 : 스크롤바 살리기
-   // $('html').css('overflow', 'auto');
- });
- let modalMain = $(".modal-main");
- //내용 배경 클릭
- modalMain.click(function (event) {
-   // 클릭 정보 전달 막기
-   event.stopPropagation();
- });
- //전체 배경 클릭
- modalWrap.click(function () {
-   modalWrap.stop().fadeOut(500);
-   // 추가기능 : 스크롤바 살리기
-   // $('html').css('overflow', 'auto');
- });
- $("html").keydown(function (key) {
-   if (key.keyCode == 13) {
-     modalWrap.stop().fadeOut(200);
-     $("html").css("overflow", "auto");
-   }
- });
-// goTop btn
-let go_top = $(".gotop");
-console.log(go_top);
-
-$(window).scroll(function () {
-  let temp = $(window).scrollTop();
-  if (temp > 500) {
-    go_top.addClass("gotop-show");
-  } else {
-    go_top.removeClass("gotop-show");
-  }
-});
-
-// gotop 눌렀을 때 최상단으로 가는거
-go_top.click(function () {
-  $("html").stop().animate(
-    {
-      scrollTop: 0,
-    },
-    1000
-  );
-});
-
-
-
-  let header = $(".header");
-  new Waypoint({
-    element: $(".header-point"),
-    handler: function (direction) {
-      if (direction == "down") {
-        header.css("top", -120);
-        header.addClass("hide");
-      } else if (direction == "up") {
-        header.css("top", 0);
-        header.removeClass("hide");
-      }
-    },
-    offset: "20%",
+  modalClose.click(function () {
+    modalWrap.stop().fadeOut(500);
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
   });
-  let scY = $(window).scrollTop(); // 스크린전체에서의 Y축의 값
-  console.log("scY", scY);
-  $(window).scroll(function () {
-    let tempY = $(window).scrollTop(); // 스크롤을 내렸을떄 나타나는 Y축의 값
-    console.log("tempY", tempY);
-
-    let temp = header.hasClass("hide");
-    /* 위 waypoint에서 스크롤을 내렸을때 hide라는 addClass를 주었기때문에  그 클래스 값으로 
-        비교하여서 header를 올릴지 내릴지 정함*/
-    if (temp == true) {
-      if (tempY - scY > 0) {
-        // 양수가 나오면 스크롤이 아래로 떨어진 의미.
-        header.css("top", -120);
-      } else {
-        header.css("top", 0);
-      }
+  let modalMain = $(".modal-main");
+  //내용 배경 클릭
+  modalMain.click(function (event) {
+    // 클릭 정보 전달 막기
+    event.stopPropagation();
+  });
+  //전체 배경 클릭
+  modalWrap.click(function () {
+    modalWrap.stop().fadeOut(500);
+    // 추가기능 : 스크롤바 살리기
+    // $('html').css('overflow', 'auto');
+  });
+  $("html").keydown(function (key) {
+    if (key.keyCode == 13) {
+      modalWrap.stop().fadeOut(200);
+      $("html").css("overflow", "auto");
     }
-    scY = tempY;
+  });
+  // goTop btn
+  let go_top = $(".gotop");
+  console.log(go_top);
+
+  $(window).scroll(function () {
+    let temp = $(window).scrollTop();
+    if (temp > 500) {
+      go_top.addClass("gotop-show");
+    } else {
+      go_top.removeClass("gotop-show");
+    }
   });
 
+  // gotop 눌렀을 때 최상단으로 가는거
+  go_top.click(function () {
+    $("html").stop().animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+  // 헤더 관련
+  let header = $(".header");
+  let scY = $(window).scrollTop();
+  let oldY;
+  $(window).scroll(function () {
+    scY = $(window).scrollTop();
+    if (scY > 101) {
+      header.addClass("hide");
+      header.addClass("hide-white");
+      $(".main-menu > li").addClass("hide-color-000");
+      if (oldY - scY < 0) {
+        // 아래로 스크롤
+        header.addClass("hide-up");
+      } else {
+        // 위로 스크롤
+        header.removeClass("hide-up");
+      }
+    } else {
+      header.removeClass("hide-up");
+      header.removeClass("hide");
+      header.removeClass("hide-white");
+      $(".main-menu > li").removeClass("hide-color-000");
+    }
+    oldY = scY;
+  });
 
   new Swiper(".sw-visual", {
     autoplay: {
@@ -119,8 +107,10 @@ go_top.click(function () {
     },
   });
 
+  let swiperActive = $(".sw-tech .swiper-slide-active");
+  let techList = $(".progress-bar-list li");
   var swiper = new Swiper(".sw-tech", {
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     // spaceBetween: 60, // 슬라이드 사이 여백
     // autoplay: {
     //   delay: 3000,
@@ -132,15 +122,18 @@ go_top.click(function () {
       nextEl: ".sw-tech-next",
       prevEl: ".sw-tech-prev",
     },
-
+    on: {
+      slideChangeTransitionStart: function () {
+        // console.log(this.realIndex);
+        techList.removeClass("progress-bar-list-active");
+        techList.eq(this.realIndex).addClass("progress-bar-list-active");
+      },
+    },
   });
 
-  let swiperActive = $('.sw-tech .swiper-slide-active');
-  let techList = $('.progress-bar-list li')
-  console.log("실시간으로 감시하는지?",swiperActive)
-  console.log(techList)
-  $.each(techList, function(index){
+  console.log("실시간으로 감시하는지?", swiperActive);
+  console.log(techList);
+  $.each(techList, function (index) {
     // swiperActive.eq(index).
-  })
+  });
 });
-
